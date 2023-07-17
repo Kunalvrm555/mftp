f6317af1e171df8c56de460faa27611a3d88c79c
Kunal Verma <kunalvrm555@gmail.com>
2023-07-17 21:35:19 +0530

    feat: send to WA client as well

--- hooks.py
+++ hooks.py
@@ -5,7 +5,10 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 import smtplib as smtp
+import base64
 from dotenv import load_dotenv
+import requests
+from html2text import html2text
 load_dotenv()
 
 
@@ -43,11 +46,31 @@ def send_email(subject, message, attachment_raw=None):
                             env['RECIPIENT_EMAIL'], msg.as_string())
     print("Email sent successfully")
 
+def send_whatsapp(notice):
+        notice['text'] = html2text(notice['text'])
+        subject = 'Notice: %s - %s' % (notice['subject'], notice['company'])
+        message = "*" + subject + "*" + "\n" + notice['text'] + "\n" + notice['time']
+        if ('attachment_url') in notice:
+            encoded_string = base64.b64encode(notice['attachment_raw'])
+            try:
+                r = requests.post(env['SEND_FILE_URL'],data={'base64string':encoded_string,'caption':message},stream=True)
+                print("Attachment sent", r.status_code)
+            except Exception as e:
+                print("Attachment not sent")
+                print(e)
+        else:
+            try:
+                r = requests.post(env['SEND_MESSAGE_URL'],data={"message":message},verify=False)
+                print("Notice sent over whatsapp", r.status_code)
+            except Exception as e:
+                print("Notice not sent over whatsapp")
+                print(e)
 
 def notices_updated(notices):
-    for notice in notices:
+    for notice in reversed(notices):
         subject = 'Notice: %s - %s' % (notice['subject'], notice['company'])
         message = '<i>(%s)</i>: <p>%s</p><br/><hr/>' % (
             notice['time'], notice['text'])
         attachment_raw = notice['attachment_raw'] if 'attachment_raw' in notice else None
         send_email(subject, message, attachment_raw)
+        send_whatsapp(notice)
