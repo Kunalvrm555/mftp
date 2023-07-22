1b961b684285cdf00f0d403b040f14058836c69f
proffapt <proffapt@pm.me>
2023-07-23 02:11:33 +0530

    feat: Support for using SSOToken across separate runs

--- update.py
+++ update.py
@@ -28,21 +28,11 @@ ERP_ATTACHMENT_URL = 'https://erp.iitkgp.ac.in/TrainingPlacementSSO/AdmFilePDF.h
 ERP_NOTICE_CONTENT_URL = 'https://erp.iitkgp.ac.in/TrainingPlacementSSO/ShowContent.jsp?year=%s&id=%s'
 
 def check_notices(session, headers):
-    _, ssoToken = erp.login(headers, session, ERPCREDS=erpcreds, OTP_CHECK_INTERVAL=2, LOGGING=True)
+    sessionToken, ssoToken = erp.login(headers, session, ERPCREDS=erpcreds, OTP_CHECK_INTERVAL=2, LOGGING=True, SESSION_STORAGE_FILE='.session_token')
+    
+    r = session.post(ERP_TPSTUDENT_URL, data=dict(ssoToken=ssoToken, menu_id=11, module_id=26), headers=headers)
     r = session.get(ERP_NOTICEBOARD_URL, headers=headers)
     r = session.get(ERP_NOTICES_URL, headers=headers)
-    
-    attachment_args = {
-        'timeout': 20,
-        'headers': {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/46.0.2490.86 Safari/537.36',
-            'Referer':
-            'https://erp.iitkgp.ac.in/SSOAdministration/login.htm?sessionToken=595794DC220159D1CBD10DB69832EF7E.worker3',
-        },
-        'verify': False
-    }
-
 
     notices_list = bs(r.text, 'html.parser')
 
