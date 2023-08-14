e05a665bae114f13cfe0f3bccbabbea710a6876c
Kunal Verma <kunalvrm555@gmail.com>
2023-08-14 22:04:57 +0530

    Exclude "PPO" notices from being sent
    This commit adds a condition to exclude notices with the word "PPO" in the subject from being sent. If a notice has "PPO" in its subject, it is skipped and a log message is printed to indicate the exclusion.

--- hooks.py
+++ hooks.py
@@ -89,6 +89,10 @@ def send_whatsapp(notice):
 
 def notices_updated(notices):
     for notice in reversed(notices):
+        # if notice['subject'] contains 'PPO', don't send it
+        if 'PPO' in notice['subject']:
+            print("PPO notice found, skipping")
+            continue
         subject = 'Notice: %s - %s' % (notice['subject'], notice['company'])
         attachment_raw = notice['attachment_raw'] if 'attachment_raw' in notice else None
         send_email(subject, notice, attachment_raw)
